bf7d836b605c4cae0086481de8334732a92a17d3
duct55600 <161632924+duct55600@users.noreply.github.com>
2024-09-15 10:43:50 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 7 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("Tổng các số từ 1 đến 100 là bội số của 7 là: " + sum);
+    }
+}
